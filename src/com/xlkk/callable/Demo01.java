745c8bd8723d7b9517f7d0dd17648661f73ea137
xlkk <60306671+xlkkqaq@users.noreply.github.com>
2022-08-09 23:06:39 +0800

    feat:Callable demo 实现

--- src/com/xlkk/callable/Demo01.java
+++ src/com/xlkk/callable/Demo01.java
@@ -11,9 +11,16 @@ import java.util.concurrent.FutureTask;
 public class Demo01 {
     public static void main(String[] args) {
         new Thread(new MyThrea01(),"AA");
-
+        /**
+         * FutureTask原理：
+         * 主线程不受影响-单开一个线程运行任务
+         * 老师上课口渴了->派一个同学去买水->上课仍然在进行->水买完直接喝
+         */
         FutureTask futureTask = new FutureTask<>(new MyThread02());
         new Thread(futureTask,"BB");
+
+
+
     }
 
 
